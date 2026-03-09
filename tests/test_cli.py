25b562b2f5d85ed42f3f04e2b0e73aeda41b5e91
aineniamh <aine.otoole@ed.ac.uk>
2026-03-09 19:35:21 +0000

    updating test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,7 +22,7 @@ def test_phylo_sets_func_callable():
     parser = build_parser()
     ns = parser.parse_args(['tree-qc', '--tree', 'tree'])
     assert callable(ns.func)
-    assert ns.tip_fields == '{sample}|{location}|{date}'
+    assert ns.tip_fields == '{id}|{location}|{date}'
     assert ns.midpoint_root is False
 
 
