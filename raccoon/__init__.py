fb73ba8bfa938d5e8000c9af0b3e8f81df97dbc9
Áine O'Toole <aine.otoole@ed.ac.uk>
2026-03-09 21:23:23 +0000

    Update version and description in __init__.py
    Updated version number to 1.0.1 and revised package description.

--- raccoon/__init__.py
+++ raccoon/__init__.py
@@ -1,10 +1,10 @@
 """
 Raccoon: Virus Genome Alignment QC Curation Package
 
-A comprehensive toolkit for curating virus genome and sub-genomic sequence alignments
+A toolkit for curating virus genome and sub-genomic sequence files, metadata alignments and phylogenetic trees,
 and flagging various QC issues including clustered SNPs, convergent SNPs, ADAR edits,
 and APOBEC3 edits.
 """
 
 _program = "raccoon"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
